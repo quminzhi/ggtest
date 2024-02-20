47d8ceb5963153ec8e3bb01e577cde2b7eaac0f3
Minzhi <quminzhi@gmail.com>
2024-02-19 17:42:03 -0800

    Added compile_commands.json

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 //Bring in the declaration. No need for include here.
-int run_app (int agc, char** argv);
+int run_app (int argc, char** argv);
 
 int main () {
     run_app(0, NULL);
--- src/run_app.cpp
+++ src/run_app.cpp
@@ -2,7 +2,17 @@
 
 #include "calculator.h"
 
-int run_app (int agc, char** argv) {
+int run_app (int argc, char** argv) {
+    if (argc < 0) {
+        return -1;
+    }
+
+    if (argv != nullptr) {
+        for (int i = 0; i < argc; i++) {
+            std::cout << argv[i] << std::endl;
+        }
+    }
+
     Calculator calc;
     std::cout << "add(10,4): " << calc.add(10,4) << std::endl;
     std::cout << "sub(10,4): " << calc.sub(10,4) << std::endl;
